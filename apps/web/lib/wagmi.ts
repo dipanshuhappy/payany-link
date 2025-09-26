3e3ff4890fd439f166452422a8fc034d0e18c5ca
dipanshuhappy <dipanshuhappy@gmail.com>
2025-09-26 23:53:09 +0530

    added ens hooks

---
+++ apps/web/lib/wagmi.ts
@@ -0,0 +1,10 @@
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+export const config = getDefaultConfig({
+  appName: "PAYANY.LINK",
+  projectId: "6b4e7efed7141d0d78856b9e23328f18",
+  chains: [mainnet],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
